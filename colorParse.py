b72437731293be99cba2c9152145da3e83b9b131
Maz <ma.mazmaz@gmail.com>
2009-07-01 16:43:14 -0400

    added ';' (oops...)

--- colorParse.py
+++ colorParse.py
@@ -6,6 +6,6 @@ for line in lines:
 	line=line.replace("\n","")
 	if line!="":
 		parts=line.split(" ")
-		code=code+"OBColor.%s=new OBColor(%s,%s,%s)\n"%(parts[0],parts[1],parts[2],parts[3])
+		code=code+"OBColor.%s=new OBColor(%s,%s,%s);\n"%(parts[0],parts[1],parts[2],parts[3])
 
 print code
