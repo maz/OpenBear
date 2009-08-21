2c7a507b93920d9c589496cee94e4b6e63c4793d
Maz <ma.mazmaz@gmail.com>
2009-08-20 22:15:16 -0400

    added copyright notices

--- ob/Debug.js
+++ ob/Debug.js
@@ -1,3 +1,20 @@
+/*
+This file is part of OpenBear.
+
+	OpenBear is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	OpenBear is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with OpenBear.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 window.OBDebug={
 	init:function OBDebug_init(){
 		window.OBDebugWin=window.open(ob.moduleUrl("ob","debugger.html"),"OBDebugWin","dependent,resizable,height=200,width=400");
