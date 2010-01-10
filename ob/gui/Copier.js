72ee529eba20948a38eb3a42e75a771ce97e0433
Maz <ma.mazmaz@gmail.com>
2010-01-10 14:01:31 -0500

    added method for requesting the user to copy text

---
+++ ob/gui/Copier.js
@@ -0,0 +1,38 @@
+OBThemeLoader.CopierInfo="copierinfo.json";
+
+window.OBCopierView=Class.create(OBView,{
+	setup:function OBCopierView_setup(txt){
+		this.txt=txt;
+		this.buffer=OBView.focused;
+		this.focus();
+		ob._tbox.value=txt;
+		ob._tbox.select();
+	},
+	keyup:function OBCopierView_keyup(evt){
+		if(this.good){
+			this.remove();
+		}else{
+			ob._tbox.value=this.txt;
+			ob._tbox.select();
+		}
+	},
+	keydown:function OBCopierView_keydown(evt){
+		if(((navigator.platform.indexOf("Mac")==-1)?evt.ctrlKey:evt.metaKey) && String.fromCharCode(evt.keyCode).toLowerCase()=="c"){
+			this.good=true;
+		}
+	},
+	redraw:function OBCopierView_redraw(){
+		this.ctx.fillStyle=OBThemeLoader.CopierInfo.backdrop;
+		this.ctx.fillRect(0,0,this.attr('width'),this.attr('height'));
+		this.ctx.fillStyle=OBThemeLoader.CopierInfo.foreground;
+		this.ctx.font=OBThemeLoader.CopierInfo.instructionsFont;
+		var size=this.ctx.measureText(this.message);
+		this.ctx.fillText(this.message,(this.attr('width')/2)-(size.attr('width')/2),(this.attr('height')/2)-(size.attr('height')/2));
+	},
+	drawFocusRing:Prototype.emptyFunction,
+	message:"Please Press "+((navigator.platform.indexOf("Mac")==-1)?"Ctrl-C":"Command-C")+" to Copy"
+});
+
+window.OBCopy=function OBCopy(txt){
+	var view=new OBCopierView(null,ob.body.attr('frame'),txt);
+}
